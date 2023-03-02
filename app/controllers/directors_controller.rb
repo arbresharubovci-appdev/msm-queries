08b65e57ac875b6abfd4f2b237ed57646d357d73
Arbreshë Rubovci <arbresharubovci@gmail.com>
2023-03-02 22:43:48 +0000

    almost done.

--- app/controllers/directors_controller.rb
+++ app/controllers/directors_controller.rb
@@ -10,11 +10,15 @@ class DirectorsController < ApplicationController
     render({:template => "director_templates/eldest.html.erb"})
   end
 
+  def youngest
+    render({:template => "director_templates/youngest.html.erb"})
+  end
+
   def director_details
     #Parameters: {"an_id"=>"42"}
     the_id = params.fetch("an_id")
     @the_director = Director.where({ :id =>  the_id }).at(0)
-    @filmography = Movie.where({ :director_id => @the_director.id })
+    @filmography = Movie.where({ :director_id =>  @the_director.id })
     render({:template => "director_templates/show.html.erb"})
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
-  get("/", { :controller => "application", :action => "homepage" })
-
+get("/", { :controller => "application", :action => "homepage" })
+get("/directors/youngest" , { :controller => "directors" , :action => "youngest"})
+get("/directors/eldest", { :controller => "directors" , :action => "wisest"})
 get("/directors" , { :controller => "directors" , :action => "index"})
 get("/directors/:an_id", {:controller => "directors" , :action => "director_details"})
-get("/directors/eldest", { :controller => "directors" , :action => "wisest"})
+
 
 end
